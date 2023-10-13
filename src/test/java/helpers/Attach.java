d41f504e20faf7da22fb182e389e4e089d99db1e
OtamKtot <119840981+OtamKtot@users.noreply.github.com>
2023-10-13 18:55:05 +0300

    improvements for HW13

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -11,6 +11,7 @@ import java.nio.charset.StandardCharsets;
 
 import static com.codeborne.selenide.Selenide.sessionId;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static java.lang.String.format;
 import static org.openqa.selenium.logging.LogType.BROWSER;
 
 public class Attach {
@@ -44,7 +45,8 @@ public class Attach {
     }
 
     public static URL getVideoUrl() {
-        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4";
+        String webDriver = System.getProperty("webDriver");
+        String videoUrl = format("%svideo/" + sessionId() + ".mp4", webDriver);
 //        System.out.println(sessionId());
         try {
             return new URL(videoUrl);
